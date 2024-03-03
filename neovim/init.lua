00757b9e879e69be2102873a6b5a16cb0498bbbd
hatimmohammed369 <hatimmohammed369@gmail.com>
2024-03-03 18:32:24 +0200

    Remove Shada and undo files: they make neovim startup slow

--- neovim/init.lua
+++ neovim/init.lua
@@ -13,19 +13,12 @@ vim.opt.compatible = false
 -- Enable plugin Termdebug
 vim.cmd('packadd termdebug')
 
--- Use shared data file
-vim.opt.shadafile = "/home/hatim/.config/nvim/shada/main.shada"
-
 -- Store swaps
 vim.opt.dir = "/home/hatim/.config/nvim/swap"
 
 -- Store backups
 vim.opt.backup = true
 vim.opt.backupdir = "/home/hatim/.config/nvim/backup"
-
--- Store undo files
-vim.opt.undofile = true
-vim.opt.undodir = "/home/hatim/.config/nvim/undo"
 
 -- Source other config files
 require('hatim.autocommands')
