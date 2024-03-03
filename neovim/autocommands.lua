aacd833154bdb5e035f07b825f0b7d6a58b72f17
hatimmohammed369 <hatimmohammed369@gmail.com>
2024-03-03 20:47:45 +0200

    Some Rust files specific options

--- neovim/autocommands.lua
+++ neovim/autocommands.lua
@@ -14,14 +14,13 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 })
 
 -- Enable `showmatch` for <> in rust files
-vim.api.nvim_create_autocmd( {"FileType"}, {
+vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = "rust",
     callback = function()
-	-- Use rust-gdb instead of gdb in plugin Termdebug
-        vim.g.termdebug_config = {command = 'rust-gdb'}
+        -- Use rust-gdb instead of gdb in plugin Termdebug
+        vim.g.termdebug_config = { command = 'rust-gdb' }
         vim.g.termdebugger = "rust-gdb"
-	-- Enable `showmatch` for <> in rust files
+        -- Enable `showmatch` for <> in rust files
         vim.opt_local.matchpairs:append("<:>")
     end
 })
-
