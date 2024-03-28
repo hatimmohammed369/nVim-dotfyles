3975e64ca8996a20f498cb1d7a14a3ec38bef31c
hatimmohammed369 <hatimmohammed369@gmail.com>
2024-03-28 16:59:07 +0200

    Center cursor in screen when scroll by pages

--- neovim/keymaps.lua
+++ neovim/keymaps.lua
@@ -22,6 +22,8 @@ vim.keymap.set('n', "J", "mzJ`z")
 -- Scroll and keep cursor in the middle of the screen
 vim.keymap.set('n', '<C-d>', "<C-d>zz")
 vim.keymap.set('n', '<C-u>', "<C-u>zz")
+vim.keymap.set('n', '<C-f>', "<C-f>zz")
+vim.keymap.set('n', '<C-b>', "<C-b>zz")
 
 -- Find next search and keep cursor in the middle of the screen
 --vim.keymap.set('n', 'n', "nzzzv")
